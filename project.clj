1de62c828f9c5056544e7d34e25b12cd97097ab3
Eric Shull <eric@exupero.org>
2015-10-26 21:33:33 -0400

    Add source paths.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/clojurescript "1.7.122"]]
   :plugins [[lein-cljsbuild "1.1.0"]
             [lein-doo "0.1.4"]]
-  ; :source-paths ["src" "test"]
+  :source-paths ["src" "test"]
   :cljsbuild {:builds {:test {:source-paths ["src" "test"]
                               :compiler {:output-to "resources/public/js/tests.js"
                                          :main "doo-macro-example.runner"
